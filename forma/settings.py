519673982d49d1cc26121bbb74cae0f2064f3ab0
Houdini <albertbuluma76@gmail.com>
2026-04-04 13:40:22 +0300

    use environment variable for secret key in production

--- forma/settings.py
+++ forma/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rwzb8(bj(u8)!@*!ijzdz(-4imxwrrd-wcfmw75o_===20_#q8'
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-rwzb8(bj(u8)!@*!ijzdz(-4imxwrrd-wcfmw75o_===20_#q8')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
